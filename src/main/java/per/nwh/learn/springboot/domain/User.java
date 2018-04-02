b9c0c684f90dfe38d4c1aa0707b3bd7869888b04
Wenhao Ni <niwenhao@gmail.com>
2018-04-03 00:42:39 +0900

    u

--- src/main/java/per/nwh/learn/springboot/domain/User.java
+++ src/main/java/per/nwh/learn/springboot/domain/User.java
@@ -2,9 +2,16 @@ package per.nwh.learn.springboot.domain;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
+@Entity
 public class User {
+    @Id @GeneratedValue
     long id;
+
     String name;
     String address;
 }
